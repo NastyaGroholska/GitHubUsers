f0be006c13b18bbbedf0afec6171c4e3ab901faf
Anastasiia <anastasiiahrokholska@gmail.com>
2023-06-16 17:49:26 +0300

    Added foreignKey to RepositoryEntity, so that deleting user from users table triggers cascade delete

--- app/src/main/java/com/ahrokholska/gitHubUsers/data/local/repositories/RepositoryEntity.kt
+++ app/src/main/java/com/ahrokholska/gitHubUsers/data/local/repositories/RepositoryEntity.kt
@@ -1,9 +1,21 @@
 package com.ahrokholska.gitHubUsers.data.local.repositories
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.ahrokholska.gitHubUsers.data.local.users.UserEntity
 
-@Entity(tableName = "repositories")
+@Entity(
+    tableName = "repositories",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["login"],
+            childColumns = ["owner"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class RepositoryEntity(
     @PrimaryKey val id: Int,
     val owner: String,
--- app/src/main/java/com/ahrokholska/gitHubUsers/data/local/users/UserEntity.kt
+++ app/src/main/java/com/ahrokholska/gitHubUsers/data/local/users/UserEntity.kt
@@ -4,4 +4,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
-data class UserEntity(@PrimaryKey val id: Int, val login: String, val pictureURL: String)
+data class UserEntity(@PrimaryKey val login: String, val id: Int, val pictureURL: String)
